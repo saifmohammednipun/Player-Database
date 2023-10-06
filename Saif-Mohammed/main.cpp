8607b149a1acfd0f32a8412156aced990e26a643
saifmohammednipun <saifmohammednipun@gmail.com>
2023-10-06 21:35:59 +0600

    Update Source Code

--- Saif-Mohammed/main.cpp
+++ Saif-Mohammed/main.cpp
@@ -85,7 +85,6 @@ void printPlayerData()
    ifstream inputFile;
    inputFile.open("/Users/saifmohammed/Desktop/Player-Database/Player-Database-CWC-2023.csv");
 
-
    if(inputFile.fail())
    {
        cout << "Error opening file" << endl;
@@ -114,68 +113,59 @@ void printPlayerData()
     while(getline(inputFile, line))
     {
 
+     try
+        {
+            stringstream ss(line);
+            getline(ss, country, ',');
+            getline(ss, payer_name, ',');
+            getline(ss, date_of_birth, ',');
 
-    try
-    {
+            getline(ss, myString, ',');
+            age = stoi(myString);
 
+            getline(ss, role, ',');
+            getline(ss, batting_style, ',');
+            getline(ss, bowling_style, ',');
 
-        stringstream ss(line);
+            getline(ss, myString, ',');
+            matches_played = stoi(myString);
 
+            getline(ss, myString, ',');
+            runs_scored = stoi(myString);
 
+            getline(ss, myString, ',');
+            wickets_taken = stoi(myString);
 
-
-        getline(ss, country, ',');
-        getline(ss, payer_name, ',');
-        getline(ss, date_of_birth, ',');
-
-        getline(ss, myString, ',');
-        age = stoi(myString);
-
-        getline(ss, role, ',');
-        getline(ss, batting_style, ',');
-        getline(ss, bowling_style, ',');
-
-        getline(ss, myString, ',');
-        matches_played = stoi(myString);
-
-        getline(ss, myString, ',');
-        runs_scored = stoi(myString);
-
-        getline(ss, myString, ',');
-        wickets_taken = stoi(myString);
-
-        getline(ss, myString, ',');
-        catches_taken = stoi(myString);
+            getline(ss, myString, ',');
+            catches_taken = stoi(myString);
 
 
-        getline(ss, myString, ',');
-        batting_average = (stof(myString));
+            getline(ss, myString, ',');
+            batting_average = (stof(myString));
 
 
 
-        getline(ss, myString, ',');
-        bowling_average =  stof((myString));
+            getline(ss, myString, ',');
+            bowling_average =  stof((myString));
 
-        getline(ss, myString, ',');
-        strike_rate = stoi(myString);
-
-
+            getline(ss, myString, ',');
+            strike_rate = stoi(myString);
     }
-    catch (exception e)
-    {
-    }
-            if(count >= 1){
-                cout << country << "\t"
-             << payer_name << "\t"
-             << date_of_birth << "\t"
-             << age << "\t"
-             << role << "\t"
-             << batting_style << "\t"
-             << bowling_style << "\t"
-             << matches_played << "\t"
-             << runs_scored << "\t"
-             << wickets_taken << "\t"
-             << catches_taken << "\t";
+    catch (exception e){}
+
+            if(count >= 1)
+            {
+             cout << country << "\t"
+                  << payer_name << "\t"
+                  << date_of_birth << "\t"
+                  << age << "\t"
+                  << role << "\t"
+                  << batting_style << "\t"
+                  << bowling_style << "\t"
+                  << matches_played << "\t"
+                  << runs_scored << "\t"
+                  << wickets_taken << "\t"
+                  << catches_taken << "\t";
 
             cout << fixed << setprecision(2); // Set precision to 2 decimal places for floating-point numbers
             cout << batting_average << "\t" << bowling_average << "\t" << strike_rate << endl;
@@ -185,7 +175,6 @@ void printPlayerData()
         count++;
     }
 }
-
 
 int main()
 {
